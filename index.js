b13cad6851fb6aaa1eb8559d74037c7f08cd6387
varunrajan <varun@thinkful.com>
2017-07-31 09:30:23 -0400

    Add initial files

---
+++ index.js
@@ -0,0 +1,105 @@
+const myObj = [
+    {
+      question: "What talent did Jessica Wicks showcase when she won the Miss Pawnee beauty pageant in 1994?",
+      answerChoices: [
+        "Eating a whole cake",
+        "Packing a suitcase",
+        "Baking muffins",
+        "Baton twirling"
+      ],
+      correct: "Packing a suitcase"
+    },
+    {
+      question: "What is Pawnee’s sister city?",
+      answerChoices: [
+        "Mexico City, Mexico",
+        "Guadalajara, Mexico",
+        "Caracas, Venezuela",
+        "Boraqua, Venezuela"
+      ],
+      correct: "Boraqua, Venezuela"
+    },
+    {
+      question: "Attribute the quote: \"I was born ready. I’m _____ f*cking ____.\"",
+      answerChoices: [
+        "Ron [f*cking] Swanson",
+        "April [f*cking] Ludgate",
+        "Andy [f*cking] Dwyer",
+        "Tom [f*cking] Haverford"
+      ],
+      correct: "Ron [f*cking] Swanson"
+    },
+/*
+ *    {
+ *     question: "Who shot Ron on the hunting trip?",
+ *     answerChoices: [
+ *       "Leslie",
+ *       "Donna",
+ *       "Tom (correct)",
+ *       "Jerry"
+ *     ],
+ *     correct: "Tom"
+ *   },
+ *   {
+ *     question: "Jerry’s real name is:",
+ *     answerChoices: [
+ *       "Larry",
+ *       "Jerry",
+ *       "Barry",
+ *       "Garry"
+ *     ],
+ *     correct: "Garry"
+ *   },
+ *   {
+ *     question: "What did R&B star and Donna’s cousin Ginuwine call his rubber ducks when he was little?",
+ *     answerChoices: [
+ *       "The Quackson Five",
+ *       "The Ducktations",
+ *       "Quack City",
+ *       "Ducks & Roses"
+ *     ],
+ *     correct: "The Quackson Five"
+ *   },
+ *   {
+ *     question: "Lil’ Sebastian is a…",
+ *     answerChoices: [
+ *       "Horse",
+ *       "Pony",
+ *       "Miniature horse",
+ *       "All of the above"
+ *     ],
+ *     correct: "Miniature horse"
+ *   },
+ *   {
+ *     question: "What’s Ben Wyatt’s favorite food?",
+ *     answerChoices: [
+ *       "Calzones",
+ *       "Pizza",
+ *       "Fruit Loops",
+ *       "Ice"
+ *     ],
+ *     correct: "Calzones"
+ *   },
+ *   {
+ *     question: "Ron Swanson’s political views can be categorized as…",
+ *     answerChoices: [
+ *       "Conservative",
+ *       "Libertarian",
+ *       "Progressive",
+ *       "Anarchist"
+ *     ],
+ *     correct: "Libertarian"
+ *   },
+ *   {
+ *     question: "What is a Pawnee resident’s preferred search engine?",
+ *     answerChoices: [
+ *       "Yahoo",
+ *       "AltaVista",
+ *       "AskJeeves",
+ *       "Bing"
+ *     ],
+ *     correct: "AltaVista"
+ *   } 
+ */
+  ]
+ 
